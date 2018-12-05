de6013e98465162ce3803be741719aedd1a22b13
zhandos <zhandos.90@gmail.com>
2018-12-05 20:23:30 +0500

    nested set stack objects with level

--- nested_set/tree_stack_obj.php
+++ nested_set/tree_stack_obj.php
@@ -6,6 +6,8 @@ class Item
 
     public $left;
     public $right;
+    public $level;
+    public $passed = false;
 
     public $children = [];
 
@@ -39,7 +41,7 @@ $treeSource = [
 $stack = $treeSource;
 
 $l = 0;
-
+$lastParentLevel = 1;
 
 while (!empty($stack)) {
     reset($stack);
@@ -47,36 +49,42 @@ while (!empty($stack)) {
     $idx = key($stack);
     $node = $stack[$idx];
 
-    $passed = $node->left !== null;
+    $passed = $node->passed;
 
     if (!$passed) {
         $node->left = ++$l;
+        $node->passed = true;
+        $node->level = $lastParentLevel;
     }
 
-    if ($passed || empty($node->children)) {
+    if (!$passed && !empty($node->children)) {
+        $lastParentLevel ++;
+        $stack = array_merge($node->children, $stack);
+    } else {
         $node->right = ++$l;
         unset($stack[$idx]);
-    } else {
-        $stack = array_merge($node->children, $stack);
+        if ($passed) {
+            $lastParentLevel--;
+        }
     }
 }
 
 
-function validateNode($node, $id, $l, $r)
+function validateNode($node, $id, $l, $r, $level)
 {
-    if ($node->id !== $id || $node->left !== $l || $node->right !== $r) {
+    if ($node->id !== $id || $node->left !== $l || $node->right !== $r || $node->level !== $level) {
         throw new ErrorException('Fail');
     }
 }
 
-validateNode($treeSource[0], 1, 1, 16);
-validateNode($treeSource[0]->children[0], 2, 2, 9);
-validateNode($treeSource[0]->children[1], 7, 10, 15);
-validateNode($treeSource[0]->children[0]->children[0], 3, 3, 8);
-validateNode($treeSource[0]->children[0]->children[0]->children[0], 4, 4, 5);
-validateNode($treeSource[0]->children[0]->children[0]->children[1], 5, 6, 7);
-validateNode($treeSource[0]->children[1]->children[0], 8, 11, 12);
-validateNode($treeSource[0]->children[1]->children[1], 9, 13, 14);
-validateNode($treeSource[1], 10, 17, 20);
-validateNode($treeSource[1]->children[0], 11, 18, 19);
+validateNode($treeSource[0], 1, 1, 16, 1);
+validateNode($treeSource[0]->children[0], 2, 2, 9, 2);
+validateNode($treeSource[0]->children[1], 7, 10, 15, 2);
+validateNode($treeSource[0]->children[0]->children[0], 3, 3, 8, 3);
+validateNode($treeSource[0]->children[0]->children[0]->children[0], 4, 4, 5, 4);
+validateNode($treeSource[0]->children[0]->children[0]->children[1], 5, 6, 7, 4);
+validateNode($treeSource[0]->children[1]->children[0], 8, 11, 12, 3);
+validateNode($treeSource[0]->children[1]->children[1], 9, 13, 14, 3);
+validateNode($treeSource[1], 10, 17, 20, 1);
+validateNode($treeSource[1]->children[0], 11, 18, 19, 2);
 
